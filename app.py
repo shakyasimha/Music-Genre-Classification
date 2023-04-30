43da5e5f64d41e37770a9742f6c8423e52563cee
Sushovan Shakya <47445119+shakyasimha@users.noreply.github.com>
2023-04-30 21:00:49 +0545

    edited app.py and index.html

--- app.py
+++ app.py
@@ -1,6 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template, request, jsonify
+import librosa 
+from keras.models import load_model
+
+UPLOAD_FOLDER = 'static/uploads'
+ALLOWED_EXTENSIONS = {'wav', 'mp3'}
 
 app = Flask(__name__)
+
+model = load_model('models/model_cat.pkl')
+
+## For classifying the music
+@app.route('/classify_music', method=['POST'])
+def classify_music():
+    audio_file = request.file['audio']
+    audio_data, _ = librosa.load(audio_file)
+    audio_features = extract_features(audio_data)
+
+    prediction = model.predict([audio_features])[0]
+    return render_template('index.html', prediction=prediction)
 
 @app.route('/')
 def home():
