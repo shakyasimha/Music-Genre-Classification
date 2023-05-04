9f4bdcc4de3618ec9f948ed60a88c4bea34d3780
Sushovan Shakya <47445119+shakyasimha@users.noreply.github.com>
2023-05-04 12:39:54 +0545

    tested out file upload in flask

--- app.py
+++ app.py
@@ -1,23 +1,62 @@
-from flask import Flask, request, render_template, request, jsonify
-import librosa 
-from keras.models import load_model
+from flask import Flask, request, render_template, request, redirect, flash, jsonify
+from werkzeug.utils import secure_filename
+import os 
 
+
+ALLOWED_EXTENSIONS = {'csv'}
 UPLOAD_FOLDER = 'static/uploads'
-ALLOWED_EXTENSIONS = {'wav', 'mp3'}
 
+# Defining the app  here
 app = Flask(__name__)
 
-model = load_model('models/model_cat.pkl')
+# Secret key of the app
+app.config['SECRET_KEY'] = '2a87446caff77077bd5d58f144784a87'
 
-## For classifying the music
-@app.route('/classify_music', method=['POST'])
-def classify_music():
-    audio_file = request.file['audio']
-    audio_data, _ = librosa.load(audio_file)
-    audio_features = extract_features(audio_data)
 
-    prediction = model.predict([audio_features])[0]
-    return render_template('index.html', prediction=prediction)
+# For storing file attributes in a dictionary
+def file_attrib(file):
+    attrib_dict = {
+        'fname': file.filename,
+        'ftype': file.content_type,
+        'fsize': file.content_length
+    }
+
+    return attrib_dict
+
+
+# For checking allowed file extension
+def allowed_extension(filename):
+    return '.' in filename and \
+        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS 
+
+
+# Route for using POST request to upload the file
+@app.route('/upload_file', methods=['POST'])
+def upload_file():
+    # Checks if the post request has the file part
+    if 'file' not in request.files:
+        flash('No file found')
+        return jsonify({'error': 'No file part'})
+    
+    file = request.files['file']
+
+    # Returns empty file without a filename if a file is not uploaded
+    if file.filename == '':
+        flash('No selected file')
+        return jsonify({'error': 'No file selected'})
+    
+    if file and allowed_extension(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+
+        attrib = file_attrib(file)
+        
+        return render_template('result.html', attrib=attrib)
+    
+    return jsonify({'error': 'Invalid file format'})
+
+
 
 @app.route('/')
 def home():
